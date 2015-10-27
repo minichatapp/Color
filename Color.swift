d6fc338438944a6b55e1c2a8f84807330a1770ea
Dondrey Taylor <ddt7@njit.edu>
2015-10-27 11:26:39 -0400

    Color module used for convert a hex string to UIColor

---
+++ Color.swift
@@ -0,0 +1,40 @@
+//
+//  Color.swift
+//  MiniChat
+//
+//  Created by Dondrey Taylor on 2/14/15.
+//  Copyright (c) 2015 MiniChat, Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Color {
+    
+    // Creates a UIColor iinstance using a hex value instead of RGB
+    static func fromHex (hex:String, alpha:Float = 1.0) -> UIColor {
+       
+        var cString:String = hex.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()).uppercaseString
+        
+        if (cString.hasPrefix("#")) 
+        {
+            cString = (cString as NSString).substringFromIndex(1)
+        }
+        
+        if (cString.characters.count != 6) 
+        {
+            return UIColor.grayColor()
+        }
+        
+        let rString = (cString as NSString).substringToIndex(2)
+        let gString = ((cString as NSString).substringFromIndex(2) as NSString).substringToIndex(2)
+        let bString = ((cString as NSString).substringFromIndex(4) as NSString).substringToIndex(2)
+        var r:CUnsignedInt = 0, g:CUnsignedInt = 0, b:CUnsignedInt = 0;
+
+        NSScanner(string: rString).scanHexInt(&r)
+        NSScanner(string: gString).scanHexInt(&g)
+        NSScanner(string: bString).scanHexInt(&b)
+        return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: CGFloat(alpha))
+    }
+    
+}
